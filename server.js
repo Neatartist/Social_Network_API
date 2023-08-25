42d083436e6790f73397b5906b433c6f2ab2ed58
Neatartist <davk0810@gmail.com>
2023-08-24 20:02:10 -0400

    update

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+// app.delete('/api/thoughts/:id', (req, res) => {
+//   const { id } = req.params;
+//   const { body } = req;
+//   console.log(body);
+//   console.log(id);
+//   res.json({ message: `delete ${id}` });
+// });
+// app.delete('/api/thoughts/:thoughtId/reactions/:reactionId', (req, res) => {
+//   const { thoughtId, reactionId } = req.params;
+//   const { body } = req;
+//   console.log(body);
+//   console.log(thoughtId);
+//   console.log(reactionId);
+//   res.json({ message: `delete ${thoughtId} ${reactionId}` });
+// });
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
